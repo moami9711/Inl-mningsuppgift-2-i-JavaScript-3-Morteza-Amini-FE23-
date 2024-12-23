2fcfe4f0786e29e5aedcb67d92632ffd9c4a9f7a
moami9711 <moami9711@gmail.com>
2024-12-23 16:05:48 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+import Board from "./components/Board";
+
+function App() {
+  return (
+    <div>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
